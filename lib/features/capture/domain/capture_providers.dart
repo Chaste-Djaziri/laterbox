673c4120c873fe29346c2286f4c7c6d6b68d7f82
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-19 08:07:43 +0200

    feat: provide iOS share receiver

--- lib/features/capture/domain/capture_providers.dart
+++ lib/features/capture/domain/capture_providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../inbox/presentation/inbox_providers.dart';
 import '../data/android_share_receiver.dart';
+import '../data/ios_share_receiver.dart';
 import 'capture_service.dart';
 
 final captureServiceProvider = Provider<CaptureService>((ref) {
@@ -10,4 +11,8 @@ final captureServiceProvider = Provider<CaptureService>((ref) {
 
 final androidShareReceiverProvider = Provider<AndroidShareReceiver>((ref) {
   return const AndroidShareReceiver();
+});
+
+final iosShareReceiverProvider = Provider<IosShareReceiver>((ref) {
+  return const IosShareReceiver();
 });
