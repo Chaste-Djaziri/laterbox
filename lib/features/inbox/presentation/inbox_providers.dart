87fb6a8f64724a83f61934c88a15687d91d68be0
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-19 06:33:19 +0200

    feat: connect inbox repository to sync

--- lib/features/inbox/presentation/inbox_providers.dart
+++ lib/features/inbox/presentation/inbox_providers.dart
@@ -1,7 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_providers.dart';
+import '../../../core/sync/sync_providers.dart';
 import '../../../shared/models/laterbox_item.dart';
+import '../data/item_repository.dart';
+
+final itemRepositoryProvider = Provider<ItemRepository>((ref) {
+  final coordinator = ref.watch(syncCoordinatorProvider);
+  return ItemRepository(
+    ref.watch(localItemDataSourceProvider),
+    onSaved: () async => coordinator.requestSync(),
+  );
+});
 
 final inboxItemsProvider = StreamProvider<List<LaterBoxItem>>((ref) {
   return ref.watch(itemRepositoryProvider).watchInboxItems();
