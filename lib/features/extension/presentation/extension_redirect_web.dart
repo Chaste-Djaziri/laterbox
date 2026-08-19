040dd7bb9b084eea8b10f50c5f9f4ebf336d6316
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-19 17:42:27 +0200

    fix(web): avoid DDC startup stall for extension callback

--- lib/features/extension/presentation/extension_redirect_web.dart
+++ lib/features/extension/presentation/extension_redirect_web.dart
@@ -1,5 +1,7 @@
-import 'package:web/web.dart' as web;
+// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
+
+import 'dart:html' as html;
 
 void redirectTo(String url) {
-  web.window.location.assign(url);
+  html.window.location.assign(url);
 }
