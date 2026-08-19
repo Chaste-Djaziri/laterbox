d1c17cfc8047071afe2b0630322fbb22aa9d011e
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-19 17:07:30 +0200

    build(extension): type connection environment

--- extension/src/env.d.ts
+++ extension/src/env.d.ts
@@ -1,5 +1,7 @@
 interface ImportMetaEnv {
   readonly VITE_CAPTURE_API_URL?: string;
+  readonly VITE_EXTENSION_CONNECT_URL?: string;
+  readonly VITE_LATERBOX_WEB_URL?: string;
 }
 
 interface ImportMeta {
