6568417cc1a29ecfd9794ad1900870459b76197d
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-19 06:33:24 +0200

    refactor: use sync aware item repository

--- lib/features/capture/presentation/capture_sheet.dart
+++ lib/features/capture/presentation/capture_sheet.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../core/database/database_providers.dart';
+import '../../inbox/presentation/inbox_providers.dart';
 
 class CaptureSheet extends ConsumerStatefulWidget {
   const CaptureSheet({super.key});
