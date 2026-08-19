065d68194ea78ccb571083355f7fd42999cf5e90
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-19 06:48:43 +0200

    feat: route app through auth gate

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../../features/inbox/presentation/inbox_screen.dart';
+import '../../features/auth/presentation/auth_gate.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
   final router = GoRouter(
     routes: [
-      GoRoute(path: '/', builder: (context, state) => const InboxScreen()),
+      GoRoute(path: '/', builder: (context, state) => const AuthGate()),
     ],
   );
   ref.onDispose(router.dispose);
