dc7aac0329290538941736e7eca7132d6553a61f
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-21 21:03:09 +0200

    feat(macos): launch main window full size using NSScreen visibleFrame

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,10 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    if let screen = NSScreen.main {
+      self.setFrame(screen.visibleFrame, display: true)
+    }
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
