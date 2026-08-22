3acce93243af78b23fc140affc5686f02e838382
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-22 10:26:40 +0200

    fix(android): update namespace and applicationId to pro.micorp.laterbox

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.laterbox"
+    namespace = "pro.micorp.laterbox"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,8 +24,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.laterbox"
+        applicationId = "pro.micorp.laterbox"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
