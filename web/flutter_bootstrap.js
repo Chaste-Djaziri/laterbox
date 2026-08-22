623980e703bd5623ed6bb0c3713226840cc39a36
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-22 12:21:54 +0200

    fix(web): explicitly disable service worker and clear caches in flutter_bootstrap.js

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -2,15 +2,24 @@
 {{flutter_build_config}}
 
 async function startLaterBox() {
-  // Flutter's generated service worker is deprecated and can keep an old app
-  // shell alive after a Pages deployment. Retire any prior registration before
-  // starting the current release.
   if ('serviceWorker' in navigator) {
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    await Promise.all(registrations.map((registration) => registration.unregister()));
+    try {
+      const registrations = await navigator.serviceWorker.getRegistrations();
+      await Promise.all(registrations.map((registration) => registration.unregister()));
+    } catch (_) {}
   }
 
-  await _flutter.loader.load();
+  if ('caches' in window) {
+    try {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((key) => caches.delete(key)));
+    } catch (_) {}
+  }
+
+  await _flutter.loader.load({
+    serviceWorkerSettings: null,
+  });
 }
 
 startLaterBox();
+
