89637da0374dfbd658ba97fb3fe4aadee66b70d2
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-08-22 10:58:08 +0200

    feat(safari): add native Safari Web Extension Xcode project

---
+++ safari_app/laterbox/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Chaste Djaziri HABIMANA HIRWA on 8/22/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
