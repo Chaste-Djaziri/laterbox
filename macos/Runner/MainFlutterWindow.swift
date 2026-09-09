5f50b437ab8fc57212bca81c7aa5b0955e88b498
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-09-10 00:49:28 +0200

    feat(macos): enable window transparency in MainFlutterWindow

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,6 +5,8 @@ class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     self.contentViewController = flutterViewController
+    self.isOpaque = false
+    self.backgroundColor = .clear
     if let screen = NSScreen.main {
       self.setFrame(screen.visibleFrame, display: true)
     }
