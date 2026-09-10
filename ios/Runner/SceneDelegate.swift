194c7664c693b32c1ff986fd68222d0640d0c92a
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-09-10 23:20:49 +0200

    fix(ios): update scene connection options type

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -5,7 +5,7 @@ class SceneDelegate: FlutterSceneDelegate {
   override func scene(
     _ scene: UIScene,
     willConnectTo session: UISceneSession,
-    options connectionOptions: UISceneConnectionOptions
+    options connectionOptions: UIScene.ConnectionOptions
   ) {
     super.scene(scene, willConnectTo: session, options: connectionOptions)
     connectShareChannel(for: scene)
