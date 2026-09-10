2d0c04fbb65844643a80515afcc146206e0ca3cb
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-09-10 23:23:23 +0200

    fix(ios): avoid duplicate implicit engine registration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,10 +10,6 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    if let controller = window?.rootViewController as? FlutterViewController {
-      registerShareChannel(with: controller.binaryMessenger)
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
