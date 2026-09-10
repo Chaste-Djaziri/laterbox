9ebe83836721be220eb9891187d59245a138ed00
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-09-11 01:04:44 +0200

    test(inbox): override guest mode and pro access in deduplication test

--- test/inbox_deduplication_test.dart
+++ test/inbox_deduplication_test.dart
@@ -5,6 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:laterbox/app.dart';
+import 'package:laterbox/core/auth/auth_provider.dart';
+import 'package:laterbox/core/billing/billing_providers.dart';
 import 'package:laterbox/core/database/app_database.dart';
 import 'package:laterbox/core/database/database_providers.dart';
 import 'package:laterbox/core/router/app_router.dart';
@@ -155,6 +157,8 @@ void main() {
       await tester.pumpWidget(
         ProviderScope(
           overrides: [
+            guestModeProvider.overrideWith((ref) => true),
+            hasProAccessProvider.overrideWithValue(true),
             appDatabaseProvider.overrideWithValue(database),
             initialLocationProvider.overrideWithValue('/inbox'),
           ],
