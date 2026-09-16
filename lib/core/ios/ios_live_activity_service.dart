26cdc42497c5c99f55d8ea48256d3c65d00bce58
Chaste Djaziri <habimanahirwa@gmail.com>
2026-09-17 01:33:16 +0200

    fix: place null-aware operator on value in IosLiveActivityService

--- lib/core/ios/ios_live_activity_service.dart
+++ lib/core/ios/ios_live_activity_service.dart
@@ -45,8 +45,8 @@ class IosLiveActivityService {
       await _channel.invokeMethod<bool>('start', {
         'id': id,
         'title': title,
-        ?'subtitle': subtitle,
-        ?'returnSchedule': returnSchedule,
+        'subtitle': ?subtitle,
+        'returnSchedule': ?returnSchedule,
         'captureType': captureType,
         'isCompleted': isCompleted,
         'isError': isError,
@@ -71,11 +71,11 @@ class IosLiveActivityService {
     try {
       await _channel.invokeMethod<bool>('update', {
         'id': id,
-        ?'title': title,
-        ?'subtitle': subtitle,
-        ?'returnSchedule': returnSchedule,
-        ?'isCompleted': isCompleted,
-        ?'isError': isError,
+        'title': ?title,
+        'subtitle': ?subtitle,
+        'returnSchedule': ?returnSchedule,
+        'isCompleted': ?isCompleted,
+        'isError': ?isError,
       });
     } on PlatformException catch (e) {
       debugPrint('[IosLiveActivityService] Failed to update Live Activity: $e');
