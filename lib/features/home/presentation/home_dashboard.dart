2b841cf836a614a86f7524231e7d31b0e93ab382
Chaste Djaziri <habimanahirwa@gmail.com>
2026-09-19 06:30:28 +0200

    fix(home): further reduce header bottom padding

--- lib/features/home/presentation/home_dashboard.dart
+++ lib/features/home/presentation/home_dashboard.dart
@@ -87,13 +87,13 @@ class _HomeDashboardState extends ConsumerState<HomeDashboard> {
       appBar: isDesktop
           ? null
           : PreferredSize(
-              preferredSize: const Size.fromHeight(155),
+              preferredSize: const Size.fromHeight(148),
               child: Container(
                 padding: EdgeInsets.fromLTRB(
                   16,
                   MediaQuery.of(context).padding.top + 8,
                   16,
-                  24,
+                  20,
                 ),
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
